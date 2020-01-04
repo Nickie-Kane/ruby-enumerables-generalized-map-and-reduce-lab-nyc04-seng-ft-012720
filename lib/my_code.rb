2f33ffc12cd824a9001a810873e1fc6645e82815
nickie kane <kameo712@gmail.com>
2020-01-04 21:11:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,16 +18,10 @@ def reduce(array, star_v=nil)
        num1 = array[0]
        i = 1
      end
-       
-       
-    new = star_v
-    i = 0 
     while i < array.length do
       new.push(yield(array[i]))
     i += 1 
     end
     new
-  else
-    
-  end
+  
 end
