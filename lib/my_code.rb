7bcb06b691eced13aaba4779ff72d5ee31015196
nickie kane <kameo712@gmail.com>
2020-01-04 20:21:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
+require 'pry'
 # Your Code Here
+def my_own_map(array)
+    result = []
+    i = 0
+    while i < array.length do
+      puts num = array[i] * -1
+      result << num
+      # binding.pry
+
+      
+      i += 1
+    end
+    return result
+end
+
+array = [1, 2, 3, -9]
