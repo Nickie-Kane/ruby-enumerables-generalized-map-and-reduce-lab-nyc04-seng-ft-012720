710e93bbd5af18e382df9a695c56155fc0a14b99
nickie kane <kameo712@gmail.com>
2020-01-04 21:17:07 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
+require 'pry'
 # Your Code Here
+def map(array)
+    new = []
+    i = 0 
+    while i < array.length do
+      new.push(yield(array[i]))
+    i += 1 
+    end
+    new
+end
+
+def reduce(array, star_v=nil)
+    if star_v
+       sum = star_v
+       i = 0
+     else
+       sum = array[0]
+       i = 1
+     end
+    while i < array.length do
+      sum = yield(sum, array[i])
+    i += 1 
+    end
+    sum
+end
